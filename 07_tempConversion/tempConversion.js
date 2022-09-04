e866d93dd631019f4f045413d2d15674dd3d1424
I-Maged <magedhanafy@gmail.com>
2022-09-05 00:08:28 +0200

    Temperature converter

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,17 @@
-const ftoc = function() {
-
+const ftoc = function (temp) {
+  let cel = (temp - 32) * (5 / 9);
+  cel = cel.toFixed(1);
+  return Number(cel);
 };
 
-const ctof = function() {
-
+const ctof = function (temp) {
+  let fehr = (temp * 9) / 5 + 32;
+  fehr = fehr.toFixed(1);
+  return Number(fehr);
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
