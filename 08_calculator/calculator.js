9ddbaeee2315cb268e30c5f1d9c5d4fab747d8fc
I-Maged <magedhanafy@gmail.com>
2022-09-17 09:29:40 +0200

    calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (numArray) {
+  let sum = 0;
+
+  numArray.forEach((number) => (sum += number));
+
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function (numArray) {
+  let sum = 1;
 
+  numArray.forEach((number) => (sum *= number));
+
+  return sum;
 };
 
-const power = function() {
-	
+const power = function (num1, num2) {
+  return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let fact = 1;
+  if (num == 0 || num == 1) {
+    return fact;
+  } else {
+    for (let i = num; i > 1; i--) {
+      fact *= i;
+    }
+    return fact;
+  }
 };
 
 // Do not edit below this line
@@ -29,5 +45,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
