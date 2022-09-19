bdbb8fe516e20969441eedb9688c408f75e048d1
I-Maged <magedhanafy@gmail.com>
2022-09-19 14:27:08 +0200

    RegExp

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const lowerCase = str.toLowerCase().replace(/[^a-z]/g, '');
 
+  const reverseStr = lowerCase.split('').reverse().join('');
+
+  return lowerCase == reverseStr;
 };
 
 // Do not edit below this line
